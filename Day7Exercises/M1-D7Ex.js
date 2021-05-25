9ecc38db0c97aea59b8a306b9d5cfaae1b435d2e
Bimal27 <58667490+Bimal27@users.noreply.github.com>
2021-05-25 16:59:36 +0200

    Day 7 Smartphone Shop Website
    Day 7 Smartphone Shop Website completed with lots of fun.

---
+++ Day7Exercises/M1-D7Ex.js
@@ -0,0 +1,14 @@
+/*EX7.: Write a function to change the h1 text*/
+  document.getElementsByTagName("h1").innerHTML= "new text to change"
+
+/*EX8.: Write a function to change the page background color*/
+document.body.style.backgroundColor = "green"
+
+/*EX9.: Write a function to change the footer address with a fake one*/
+
+
+/*EX10.: Write a function to add a CSS class to every Amazon link*/
+
+/*EX11: Write a function to toggle a CSS class for all the images in the table; that class should set the visibility of the image*/
+
+/*EX12: Write a function to color the price of each product in a different one every time it's invoked*/
